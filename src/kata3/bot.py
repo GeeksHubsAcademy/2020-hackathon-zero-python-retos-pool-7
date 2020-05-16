eb612b051b4a44fe5e77de51ca2c08bdba7626aa
Rodrigo Quispe <roquispelaura@gmail.com>
2020-05-16 13:31:20 -0500

    PR-Reto-3

--- src/kata3/bot.py
+++ src/kata3/bot.py
@@ -12,41 +12,49 @@ logger = logging.getLogger(__name__)
 # Definimos algunas funciones para los comandos. Estos generalmente toman los dos argumentos update y context
 def start(update, context):
     """Envia un mensaje cuando se emita el comando /start."""
-    return ""
+    return update.message.reply_text('Hola, Geeks!')
+
 
 def help(update, context):
     """Envia un mensaje cuando se emita el comando /help."""
-    return ""
+    return update.message.reply_text('Ayudame!')
+
 
 def mayus(update, context):
-        #
-        return ""
+    textUpper = " ".join(map(lambda x:x.upper(), context.args))
+    return update.message.reply_text(textUpper)
+
 
 def alreves(update, context):
     """Repite el mensaje del usuario."""
-    #
-    return ""
+    textReverse = update.message.text[::-1]
+    message = update.message.reply_text(textReverse)
+
+    return message
+
 
 def error(update, context):
     """Envia los errores por consola"""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+
 def main():
     """Inicio del Bot"""
-    #Colocamos el Token creado por FatherBot
-    updater = Updater("", use_context=True)
+    # Colocamos el Token creado por FatherBot
+    updater = Updater(
+        "1128656921:AAFAGXhcCwDeySb8nz8Jx37Ng3a2jN6eG88", use_context=True)
 
     # Es el Registro de Comandos a través del dispartcher
-    dp = #
+    dp = updater.dispatcher
 
     # Añadimos a la lista de Registro todos los comandos con su función [start - help - mayus]
-    #
-    #
-    #
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("mayus", mayus))
 
     # Este comando es un Trigger que se lanza cuando no hay comandos [alreves]
-    #
-    
+    dp.add_handler(MessageHandler(Filters.text, alreves))
+
     # Y este espera al error
     dp.add_error_handler(error)
 
